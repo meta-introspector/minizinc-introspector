MiniZinc/libminizinc

commit 2113830a4095e989cdef42b00dee65d464f680ed

Fix short-circuit evaluation of bool expressions, and make deopt(<>) return undefined instead of crash

lib/eval_par.cpp
@@ -595,40 +595,33 @@ namespace MiniZinc {
     case Expression::E_BINOP:
       {
         BinOp* bo = e->cast<BinOp>();
-        Expression* lhs = NULL;
-        Expression* rhs = NULL;
-        if (bo->type()==Type::parbool()) {
-          try {
-            lhs = eval_par(env, bo->lhs());
-            rhs = eval_par(env, bo->rhs());
-          } catch (ResultUndefinedError& e) {
-            return false;
-          }
-        } else {
-          lhs = eval_par(env, bo->lhs());
-          rhs = eval_par(env, bo->rhs());
-        }
+        Expression* lhs = bo->lhs();
+        Expression* rhs = bo->rhs();
         if ( bo->op()==BOT_EQ && (lhs->type().isopt() || rhs->type().isopt()) ) {
           if (lhs == constants().absent || rhs==constants().absent)
             return lhs==rhs;
         }
         if (lhs->type().isbool() && rhs->type().isbool()) {
-          switch (bo->op()) {
-          case BOT_LE: return eval_bool(env,lhs)<eval_bool(env,rhs);
-          case BOT_LQ: return eval_bool(env,lhs)<=eval_bool(env,rhs);
-          case BOT_GR: return eval_bool(env,lhs)>eval_bool(env,rhs);
-          case BOT_GQ: return eval_bool(env,lhs)>=eval_bool(env,rhs);
-          case BOT_EQ: return eval_bool(env,lhs)==eval_bool(env,rhs);
-          case BOT_NQ: return eval_bool(env,lhs)!=eval_bool(env,rhs);
-          case BOT_EQUIV: return eval_bool(env,lhs)==eval_bool(env,rhs);
-          case BOT_IMPL: return (!eval_bool(env,lhs))||eval_bool(env,rhs);
-          case BOT_RIMPL: return (!eval_bool(env,rhs))||eval_bool(env,lhs);
-          case BOT_OR: return eval_bool(env,lhs)||eval_bool(env,rhs);
-          case BOT_AND: return eval_bool(env,lhs)&&eval_bool(env,rhs);
-          case BOT_XOR: return eval_bool(env,lhs)^eval_bool(env,rhs);
-          default:
-            assert(false);
-            throw EvalError(env, e->loc(),"not a bool expression", bo->opToString());
+          try {
+            switch (bo->op()) {
+            case BOT_LE: return eval_bool(env,lhs)<eval_bool(env,rhs);
+            case BOT_LQ: return eval_bool(env,lhs)<=eval_bool(env,rhs);
+            case BOT_GR: return eval_bool(env,lhs)>eval_bool(env,rhs);
+            case BOT_GQ: return eval_bool(env,lhs)>=eval_bool(env,rhs);
+            case BOT_EQ: return eval_bool(env,lhs)==eval_bool(env,rhs);
+            case BOT_NQ: return eval_bool(env,lhs)!=eval_bool(env,rhs);
+            case BOT_EQUIV: return eval_bool(env,lhs)==eval_bool(env,rhs);
+            case BOT_IMPL: return (!eval_bool(env,lhs))||eval_bool(env,rhs);
+            case BOT_RIMPL: return (!eval_bool(env,rhs))||eval_bool(env,lhs);
+            case BOT_OR: return eval_bool(env,lhs)||eval_bool(env,rhs);
+            case BOT_AND: return eval_bool(env,lhs)&&eval_bool(env,rhs);
+            case BOT_XOR: return eval_bool(env,lhs)^eval_bool(env,rhs);
+            default:
+              assert(false);
+              throw EvalError(env, e->loc(),"not a bool expression", bo->opToString());
+            }
+          } catch (ResultUndefinedError&) {
+            return false;
           }
         } else if (lhs->type().isint() && rhs->type().isint()) {
           try {
@@ -1370,8 +1363,11 @@ namespace MiniZinc {
               if (c->decl()->_builtins.e) {
                 return eval_par(env,c->decl()->_builtins.e(env,c));
               } else {
-                if (c->decl()->e()==NULL)
+                if (c->decl()->e()==NULL) {
+                  if (c->id()=="deopt" && Expression::equal(c->args()[0],constants().absent))
+                    throw ResultUndefinedError(env, e->loc(), "deopt(<>) is undefined");
                   return c;
+                }
                 return eval_call<EvalPar>(env,c);
               }
             } else {

lib/flatten.cpp
@@ -4736,8 +4736,13 @@ namespace MiniZinc {
               Type callt = decl->rtype(env,argt);
               if (callt.ispar() && callt.bt()!=Type::BT_ANN) {
                 GCLock lock;
-                ret.b = conj(env,b,Ctx(),args_ee);
-                ret.r = bind(env,ctx,r,eval_par(env,cr_c));
+                try {
+                  ret.r = bind(env,ctx,r,eval_par(env,cr_c));
+                  ret.b = conj(env,b,Ctx(),args_ee);
+                } catch (ResultUndefinedError&) {
+                  ret.b = bind(env,Ctx(),b,constants().lit_false);
+                  return ret;
+                }
                 // Do not insert into map, since par results will quickly become
                 // garbage anyway and then disappear from the map
               } else if (decl->_builtins.e) {

share/minizinc/std/stdlib.mzn
@@ -417,13 +417,13 @@ function var opt int: bool2int(var opt bool: x) ::promise_total =
   both are present and have the same value. */
 predicate int_eq(var opt int: x, var opt int: y) =
      (absent(x) /\ absent(y))
-  \/ (occurs(x) /\ occurs(y) /\ deopt(x)=deopt(y));
+  \/ (occurs(x) /\ occurs(y) /\ (deopt(x)=deopt(y))::maybe_partial);
 
 /** @group optiontypes.int True iff only one of \a x and \a y is absent or
   both are present and have different values. */
 predicate int_ne(var opt int : x, var opt int : y) =
      (absent(x) != absent(y))
-  \/ (occurs(x) /\ occurs(y) /\ deopt(x)!=deopt(y));
+  \/ (occurs(x) /\ occurs(y) /\ (deopt(x)!=deopt(y))::maybe_partial);
 
 /** @group optiontypes.int Return sum of non-absent elements of \a x. */
 function var int: sum(array[int] of var opt int: x) =

tests/unit/evaluation/minizinc/regression/error_in_comprehension.err_exp2
@@ -1,0 +1,8 @@
+MiniZinc: result of evaluation is undefined: 
+  unit/evaluation/minizinc/regression/error_in_comprehension.mzn:2:
+  in variable declaration for 'neighbours'
+  unit/evaluation/minizinc/regression/error_in_comprehension.mzn:3:
+  in set comprehension expression
+  in set literal
+  in array access
+  array access out of bounds

meta-introspector/minizinc-introspector

commit 83ba4fa6345f02aa17be65c9aea1332fa7877910

Need to evaluate arguments of boolean BinOps if their types are top (i.e. if they come from a polymorphic function argument). Fixes #32. This bug was closed earlier but 2113830a4095e989cdef42b00dee65d464f680ed introduced a regression while trying to fix short-circuit evaluation for boolean connectives.

lib/eval_par.cpp
@@ -857,7 +857,11 @@ namespace MiniZinc {
         {
           BinOp* bo = e->cast<BinOp>();
           Expression* lhs = bo->lhs();
+          if (lhs->type().bt()==Type::BT_TOP)
+            lhs = eval_par(env,lhs);
           Expression* rhs = bo->rhs();
+          if (rhs->type().bt()==Type::BT_TOP)
+            rhs = eval_par(env,rhs);
           if ( bo->op()==BOT_EQ && (lhs->type().isopt() || rhs->type().isopt()) ) {
             if (lhs == constants().absent || rhs==constants().absent)
               return lhs==rhs;