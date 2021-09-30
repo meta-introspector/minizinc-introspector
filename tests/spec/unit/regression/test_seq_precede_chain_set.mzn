MiniZinc/libminizinc

commit de5d90c772f68e66fb7400e039bce01a2d93b3da

More splitting of files for reification

share/minizinc/std/fzn_seq_precede_chain_set.mzn
@@ -1,12 +1,2 @@
-predicate fzn_seq_precede_chain_set(array[int] of var set of int: X) =
-    let {
-        set of int: S = ub_array(X); % set of possible values
-        int : l = min (S) ;     % least possible value
-        int : u = max (S) ;     % greatest possible value
-        int : f = min ( index_set (X ));
-        array [ index_set (X) ] of var l .. u: H;
-    } in
-        H[f] <= 1 /\ H[f] = max (X[f] union {0}) /\
-        forall ( i in index_set (X) diff {f} ) (
-            H[i] <= H[i-1] + 1 /\ H[i] = max (max(X[i]), H[i-1])
-        );
+predicate fzn_seq_precede_chain_set_reif(array[int] of var set of int: X, var bool: b) =
+  abort("Reified seq_precede_chain constraint is not supported");

share/minizinc/std/fzn_seq_precede_chain_set_reif.mzn
@@ -1,0 +1,2 @@
+predicate fzn_seq_precede_chain_set(array[int] of var set of int: X) =
+  abort("Reified seq_precede_chain constraint is not supported");

share/minizinc/std/fzn_dsteiner_reif.mzn
@@ -1,0 +1,5 @@
+predicate fzn_dsteiner_reif(int: N, int: E, array[int] of int: from, array[int] of int: to, array[int] of int: w,
+                            var int: r, array[int] of var bool: ns, array[int] of var bool: es, var int: K, var bool: b) =
+  abort("Reified steiner constraint is not supported");
+   
+%-----------------------------------------------------------------------------%

share/minizinc/std/fzn_dtree_enum_reif.mzn
@@ -1,0 +1,6 @@
+predicate fzn_dtree_reif(array[int] of $$N: from, array[int] of $$N: to,
+                         var $$N: r, array[$$N] of var bool: ns, array[int] of var bool: es,
+                         var bool: b) =
+  abort("Reified dtree constraint is not supported");
+
+%-----------------------------------------------------------------------------%

share/minizinc/std/fzn_dtree_int_reif.mzn
@@ -1,0 +1,5 @@
+predicate fzn_dtree_reif(int: N, int: E, array[int] of int: from, array[int] of int: to,
+                         var int: r, array[int] of var bool: ns, array[int] of var bool: es, var bool: b) =
+  abort("Reified dtree constraint is not supported");
+
+%-----------------------------------------------------------------------------%

share/minizinc/std/fzn_dwst_reif.mzn
@@ -1,0 +1,5 @@
+predicate fzn_dwst_reif(int: N, int: E, array[int] of int: from, array[int] of int: to, array[int] of int: w,
+                        var int: r, array[int] of var bool: es, var int: K, var bool: b) =
+  abort("Reified dwst constraint is not supported");
+   
+%-----------------------------------------------------------------------------%

share/minizinc/std/fzn_seq_precede_chain_int_reif.mzn
@@ -1,0 +1,2 @@
+predicate fzn_seq_precede_chain_int_reif(array[int] of var int: X, var bool: b) =
+  abort("Reified seq_precede_chain constraint is not supported");

share/minizinc/std/fzn_set_member_reif.mzn
@@ -1,9 +1,6 @@
-include "fzn_set_member.mzn";
-include "fzn_set_member_reif.mzn";
-
 %-----------------------------------------------------------------------------%
 % Requires that 'y' occurs in the array or set 'x'.
 %-----------------------------------------------------------------------------%
 
-predicate set_member(var set of int: x, var int: y) =
-    fzn_set_member(x, y);
+predicate fzn_set_member_reif(var set of int: x, var int: y, var bool: b) =
+    b <-> y in x;

meta-introspector/minizinc-introspector

commit 2b612d9f51a544ddf62b60936a90fb25d27977b2

Fix non-reified decomposition of seq_precede_chain for sets
(accidentally broken in de5d90c772f68e66fb7400e039bce01a2d93b3da)

share/minizinc/std/fzn_seq_precede_chain_set.mzn
@@ -1,2 +1,12 @@
-predicate fzn_seq_precede_chain_set_reif(array[int] of var set of int: X, var bool: b) =
-  abort("Reified seq_precede_chain constraint is not supported");
+predicate fzn_seq_precede_chain_set(array[int] of var set of int: X) =
+    let {
+        set of int: S = ub_array(X); % set of possible values
+        int : l = min (S) ;     % least possible value
+        int : u = max (S) ;     % greatest possible value
+        int : f = min ( index_set (X ));
+        array [ index_set (X) ] of var l .. u: H;
+    } in
+        H[f] <= 1 /\ H[f] = max (X[f] union {0}) /\
+        forall ( i in index_set (X) diff {f} ) (
+            H[i] <= H[i-1] + 1 /\ H[i] = max (max(X[i]), H[i-1])
+        );

share/minizinc/std/fzn_seq_precede_chain_set_reif.mzn
@@ -1,2 +1,2 @@
-predicate fzn_seq_precede_chain_set(array[int] of var set of int: X) =
+predicate fzn_seq_precede_chain_set_reif(array[int] of var set of int: X) =
   abort("Reified seq_precede_chain constraint is not supported");

tests/spec/unit/regression/test_seq_precede_chain_set.mzn
@@ -1,0 +1,20 @@
+/***
+!Test
+expected:
+- !Result
+  solution: !Solution
+    x: [!!set {1}, !!set {1}]
+- !Result
+  solution: !Solution
+    x: [!!set {1}, !!set {2}]
+***/
+
+% Regression test for accidental removal of decomposition from fzn_seq_precede_chain_set
+
+include "globals.mzn";
+
+array [1..2] of var set of 1..3: x;
+
+constraint forall (x_i in x) (card(x_i) = 1);
+
+constraint seq_precede_chain(x);