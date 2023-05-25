MiniZinc/libminizinc

commit c8fdf078b25b43bbb1553a1cc6d4b96931d3da9c

Fix incorrect context being used when flattening some binary operators

lib/flatten/flatten_binop.cpp
@@ -1381,7 +1381,7 @@ EE flatten_bool_op(EnvI& env, Ctx& ctx, const Ctx& ctx0, const Ctx& ctx1, Expres
         }
         ret.r = flat_exp(env, ctx, cc, r, ctx.partialityVar(env)).r;
       } else {
-        ees[2].b = flat_exp(env, Ctx(), cc, nullptr, env.constants.varTrue).r;
+        ees[2].b = flat_exp(env, ctx, cc, nullptr, env.constants.varTrue).r;
         if (doubleNeg) {
           GCLock lock;
           Type t = Expression::type(ees[2].b());

tests/spec/unit/regression/github_683.mzn
@@ -1,0 +1,18 @@
+/***
+!Test
+solvers: [gecode]
+expected: 
+  - !Result
+    solution: !Solution
+      vars: [2, 1]
+      c: 1
+  - !Result
+    solution: !Solution
+      vars: [1, 2]
+      c: 1
+***/
+array[1..2] of opt 1..1: values = [1, <>];
+array[1..2] of var 1..2: vars;
+function var 1..2: identity(var 1..2: k) = (k - 1) mod 2 + 1;
+var 1..2: c ::output = count(x in 1..2)(values[identity(vars[x])] = 1) ::mzn_break_here;
+constraint c = 1;

meta-introspector/minizinc-introspector

commit 0d56e368de6ae86fa8f5ecdb7a2971019fb1afd9

Fix regression in the context determination of negated binary operators
Caused by changes made in c8fdf078b25b43bbb1553a1cc6d4b96931d3da9c which
did not account for the fact that the operator might be negated.

Resolves #695 on GitHub

lib/flatten/flatten_binop.cpp
@@ -1402,14 +1402,15 @@ EE flatten_bool_op(EnvI& env, Ctx& ctx, const Ctx& ctx0, const Ctx& ctx1, Expres
       }
       KeepAlive ka(cc);
       GC::unlock();
+      Ctx ccCtx = ctx;
+      if (doubleNeg) {
+        ccCtx.b = -ctx.b;
+        ccCtx.neg = !ctx.neg;
+      }
       if (singleExp) {
-        if (doubleNeg) {
-          ctx.b = -ctx.b;
-          ctx.neg = !ctx.neg;
-        }
-        ret.r = flat_exp(env, ctx, cc, r, ctx.partialityVar(env)).r;
+        ret.r = flat_exp(env, ccCtx, cc, r, ctx.partialityVar(env)).r;
       } else {
-        ees[2].b = flat_exp(env, ctx, cc, nullptr, env.constants.varTrue).r;
+        ees[2].b = flat_exp(env, ccCtx, cc, nullptr, env.constants.varTrue).r;
         if (doubleNeg) {
           GCLock lock;
           Type t = Expression::type(ees[2].b());

tests/spec/unit/regression/github_695.mzn
@@ -1,0 +1,41 @@
+/***
+!Test
+solvers: [gecode]
+options:
+  all_solutions: true
+expected: !Result
+  status: ALL_SOLUTIONS
+  solution: !SolutionSet
+  - !Solution
+    x: -3
+    y: -3
+    idx: 0
+    val: -3
+    p: true
+  - !Solution
+    x: -3
+    y: -3
+    idx: 1
+    val: -3
+    p: true
+  - !Solution
+    x: -3
+    y: -3
+    idx: 2
+    val: -3
+    p: false
+  - !Solution
+    x: -3
+    y: -3
+    idx: 2
+    val: -3
+    p: true
+***/
+
+var -3..-3: x;
+var -3..-3: y;
+var 0..2: idx;
+var -3..-2: val;
+var bool: p;
+constraint ([0: x, y][idx] = val) -> p;
+constraint val = max([x,y]);