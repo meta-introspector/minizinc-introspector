2e0dd5539f488bb5fb1db01be7a5fe1687015ef8
mike dupont <h4@solfunmeme.com>
2025-08-13 11:01:14 -0400

    wip

--- scripts/run_embedding_model.sh
+++ scripts/run_embedding_model.sh
@@ -3,10 +3,15 @@
 # Script to run the embedding_sphere.mzn model
 
 LIBMINIZINC_BUILD_DIR="/data/data/com.termux/files/home/storage/github/libminizinc/build"
-MODEL_PATH="/data/data/com.termux/files/home/storage/github/libminizinc/embedding_sphere.mzn"
-DATA_PATH="/data/data/com.termux/files/home/storage/github/libminizinc/example.dzn"
+MODEL_PATH="/data/data/com.termux/files/home/storage/github/libminizinc/embedding_sphere_optimized.mzn"
+DATA_DIR="/data/data/com.termux/files/home/storage/github/libminizinc"
 
-"$LIBMINIZINC_BUILD_DIR/minizinc" "$MODEL_PATH" "$DATA_PATH"
+"$LIBMINIZINC_BUILD_DIR/minizinc" "$MODEL_PATH" \
+    "$DATA_DIR/example_core_params.dzn" \
+    "$DATA_DIR/example_kappa_params.dzn" \
+    "$DATA_DIR/example_other_params.dzn" \
+    "$DATA_DIR/example_relations.dzn" \
+    "$DATA_DIR/example_vector_params.dzn"
 
 if [ $? -ne 0 ]; then
     echo "MiniZinc model run failed!"
