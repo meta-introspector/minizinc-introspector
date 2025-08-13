44578fb2eeb0db3c05959db263b12b3e95c6e1bf
mike dupont <h4@solfunmeme.com>
2025-08-13 12:01:38 -0400

    wow

--- scripts/run_embedding_model.sh
+++ scripts/run_embedding_model.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Script to run the embedding_sphere.mzn model
+# Script to run a MiniZinc model
 
 # Source the environment variables
 source "$(dirname "$0")/../.env"
 
-MODEL_PATH="${MINIZINC_MODELS_DIR}/embedding_sphere_final.mzn"
+# Check if a model path is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <model_file.mzn>"
+    exit 1
+fi
 
-"$LIBMINIZINC_BUILD_DIR/minizinc" "$MODEL_PATH" \
+MODEL_FILE="$1"
+
+"$LIBMINIZINC_BUILD_DIR/minizinc" "$MODEL_FILE" \
     "${MINIZINC_DATA_DIR}/example_core_params.dzn" \
     "${MINIZINC_DATA_DIR}/example_kappa_params.dzn" \
     "${MINIZINC_DATA_DIR}/example_other_params.dzn" \
