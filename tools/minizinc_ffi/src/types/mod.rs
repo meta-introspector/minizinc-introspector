871f1ab1f60b8ea7aa215b478ea067f7f1d3ade3
mike dupont <h4@solfunmeme.com>
2025-08-15 19:09:34 -0400

    Fix: Add #[repr(C)] to FFI-bound structs in Rust
    - Added `#[repr(C)]` to all structs in `tools/minizinc_ffi/src/types/mod.rs` that are passed across the FFI boundary.
    - This addresses the `improper_ctypes` warnings and ensures correct memory layout when interacting with the C ABI.

--- tools/minizinc_ffi/src/types/mod.rs
+++ tools/minizinc_ffi/src/types/mod.rs
@@ -1,23 +1,42 @@
+#[repr(C)]
 pub struct MiniZincModel(pub *mut std::os::raw::c_void);
+#[repr(C)]
 pub struct MiniZincItem(pub *mut std::os::raw::c_void);
+#[repr(C)]
 pub struct MiniZincEnvWrapper(pub *mut std::os::raw::c_void);
+#[repr(C)]
 pub struct MiniZincSolveItem(pub *mut std::os::raw::c_void);
+#[repr(C)]
 pub struct MiniZincOutputItem(pub *mut std::os::raw::c_void);
+#[repr(C)]
 pub struct MiniZincAssignItem(pub *mut std::os::raw::c_void);
+#[repr(C)]
 pub struct MiniZincConstraintItem(pub *mut std::os::raw::c_void);
+#[repr(C)]
 pub struct MiniZincIncludeItem(pub *mut std::os::raw::c_void);
+#[repr(C)]
 pub struct MiniZincFunctionItem(pub *mut std::os::raw::c_void);
+#[repr(C)]
 pub struct MiniZincFloatLit(pub *mut std::os::raw::c_void);
+#[repr(C)]
 pub struct MiniZincSetLit(pub *mut std::os::raw::c_void);
+#[repr(C)]
 pub struct MiniZincBoolLit(pub *mut std::os::raw::c_void);
+#[repr(C)]
 pub struct MiniZincStringLit(pub *mut std::os::raw::c_void);
+#[repr(C)]
 pub struct MiniZincId(pub *mut std::os::raw::c_void);
+#[repr(C)]
 pub struct MiniZincAnonVar(pub *mut std::os::raw::c_void);
+#[repr(C)]
 pub struct MiniZincArrayLit(pub *mut std::os::raw::c_void);
 #[repr(C)]
 pub struct MznSolver { _data: [u8; 0] }
+#[repr(C)]
 #[derive(Debug)]
 pub struct MiniZincExpression(pub *mut std::os::raw::c_void);
+#[repr(C)]
 pub struct MiniZincVarDecl(pub *mut std::os::raw::c_void);
+#[repr(C)]
 #[derive(Debug)]
 pub struct MiniZincTypeInst(pub *mut std::os::raw::c_void);
