a048c66a03344f08832e793a94802f22c65df2d4
mike dupont <h4@solfunmeme.com>
2025-08-15 07:21:46 -0400

    wip

---
+++ tools/minizinc_ffi/src/array_lit.rs
@@ -0,0 +1,14 @@
+impl MiniZincArrayLit {
+    pub fn size(&self) -> u32 {
+        unsafe { arraylit_get_size(self.0) }
+    }
+
+    pub fn get_element_at_index(&self, index: u32) -> Option<MiniZincExpression> {
+        let expr_ptr = unsafe { arraylit_get_element_at_index(self.0, index) };
+        if expr_ptr.is_null() {
+            None
+        } else {
+            Some(MiniZincExpression(expr_ptr))
+        }
+    }
+}
