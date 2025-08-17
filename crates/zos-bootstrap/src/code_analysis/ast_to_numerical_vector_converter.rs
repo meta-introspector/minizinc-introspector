b9a9231855bc4f05562cbc74e03e58074490517c
mike dupont <h4@solfunmeme.com>
2025-08-17 18:26:29 -0400

    wip

--- crates/zos-bootstrap/src/code_analysis/ast_to_numerical_vector_converter.rs
+++ crates/zos-bootstrap/src/code_analysis/ast_to_numerical_vector_converter.rs
@@ -1,4 +1,4 @@
-use syn::{visit::{self, Visit}, File, Item, Ident, Lit, Expr};
+use syn::{visit::{self, Visit}, File, Item, Ident, Lit, Expr, FnArg, PatType};
 
 use crate::code_analysis::numerical_vector_generator::{get_prime_for_vocabulary, compose_numerical_vector};
 
@@ -7,6 +7,8 @@ pub enum AstElement {
     Crate(String),
     Module(String),
     Function(String),
+    FunctionArg(String),
+    FunctionArgType(String),
     Struct(String),
     Enum(String),
     Const(String),
@@ -22,6 +24,8 @@ impl AstElement {
             AstElement::Crate(s) => format!("crate_{}", s),
             AstElement::Module(s) => format!("module_{}", s),
             AstElement::Function(s) => format!("fn_{}", s),
+            AstElement::FunctionArg(s) => format!("fn_arg_{}", s),
+            AstElement::FunctionArgType(s) => format!("fn_arg_type_{}", s),
             AstElement::Struct(s) => format!("struct_{}", s),
             AstElement::Enum(s) => format!("enum_{}", s),
             AstElement::Const(s) => format!("const_{}", s),
@@ -97,6 +101,25 @@ impl<'ast> Visit<'ast> for AstToNumericalVectorConverter {
                 let old_function_name = self.current_function_name.take();
                 self.current_function_name = Some(item_fn.sig.ident.to_string());
                 self.add_element(AstElement::Function(item_fn.sig.ident.to_string()));
+
+                // Extract function arguments and their types
+                for input in &item_fn.sig.inputs {
+                    match input {
+                        FnArg::Receiver(_) => {
+                            // `self` argument, can be ignored or handled specifically
+                            self.add_element(AstElement::FunctionArg("self".to_string()));
+                            self.add_element(AstElement::FunctionArgType("self_type".to_string())); // Placeholder for self type
+                        },
+                        FnArg::Typed(PatType { pat, ty, .. }) => {
+                            if let Pat::Ident(pat_ident) = &**pat {
+                                self.add_element(AstElement::FunctionArg(pat_ident.ident.to_string()));
+                            }
+                            // Extract type string
+                            self.add_element(AstElement::FunctionArgType(ty.to_token_stream().to_string()));
+                        }
+                    }
+                }
+
                 visit::visit_item_fn(self, item_fn);
                 self.current_function_name = old_function_name;
             }
